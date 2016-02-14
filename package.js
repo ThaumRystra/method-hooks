7505c8337522d33fc2743eecc144bbc13cd2240b
Leonidez Acosta <leonidez@poeticsystems.com>
2016-02-13 23:20:05 -0600

    create version 1.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lacosta:method-hooks',
-  version: '0.0.1',
+  version: '1.0.0',
   summary: 'Provide before and after hooks to methods created via mdg:validated-method',
   git: 'https://github.com/leonidez/method-hooks',
   documentation: 'README.md'
@@ -25,7 +25,7 @@ Package.onTest(function(api) {
   api.use([
     'ecmascript',
     'stevezhu:lodash@4.1.0',
-    'lacosta:method-hooks@0.0.1',
+    'lacosta:method-hooks@1.0.0',
     'sanjo:jasmine@0.21.0',
     'velocity:console-reporter@0.1.4',
     'velocity:helpers@0.5.0'
